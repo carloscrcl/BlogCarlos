dafeac73a64f558f750ca10bc27b6bf3f93f0dba
Carlos Callejas <carloscrcl@gmail.com>
2022-07-14 23:44:02 -0500

    1. Se agrega la funcionalidad de muestra previa de la imagen seleccionada, asi mismo se actualiza cunado se combia la misma. 2. Se agrega la creacion de la base de datos blog_practica

--- views/footer.php
+++ views/footer.php
@@ -1,7 +1,23 @@
 <footer>
-        <p class="copyright">Copyright @ 2022 - Carlos R. Callejas</p>
+  <p class="copyright">Copyright @ 2022 - Carlos R. Callejas</p>
 </footer>
+<script>
+let vistaPrevia = (event) => {
+  let leer_img = new FileReader();
+  //   console.log(leer_img);
+  let id_img = document.getElementById('img-previa');
 
+  leer_img.onload = () => {
+    if (leer_img.readyState == 2) {
+      id_img.src = leer_img.result;
+      //       console.log(id_img);
+    }
+  }
+  console.log(leer_img);
+  leer_img.readAsDataURL(event.target.files[0]);
+}
+</script>
 
 </body>
+
 </html>
--- views/nuevo.view.php
+++ views/nuevo.view.php
@@ -4,9 +4,14 @@
   <div class="post">
     <article>
       <h2 class="titulo">Nuevo Articulo</h2>
+      <div class="contentImage">
+        <img src="" alt="" id="img-previa">
+      </div>
       <form class="formulario" method="POST" action=" <?php echo htmlspecialchars($_SERVER['PHP_SELF']) ;?>"
         enctype="multipart/form-data">
-        <input type="file" name="thumb" placeholder="thumb">
+        <div class="file_select">
+          <input class="files" type="file" name="thumb" placeholder="thumb" onchange="vistaPrevia(event)">
+        </div>
         <input type="text" name="titulo" placeholder="titulo del articulo">
         <input type="text" name="extracto" placeholder="Extracto">
 
